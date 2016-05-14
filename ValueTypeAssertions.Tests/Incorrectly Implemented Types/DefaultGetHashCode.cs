47e26bbdb61b8012387acbefc1e39c55865bf917
Jay Bazuzi <JAY@BAZUZI.COM>
2016-05-14 14:52:14 -0700

    DefaultGetHashCode

---
+++ ValueTypeAssertions.Tests/Incorrectly Implemented Types/DefaultGetHashCode.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (C) 2016 Jay Bazuzi - This software may be modified and distributed under the terms of the MIT license.  See the LICENSE.md file for details.
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ValueTypeAssertions.Tests.Incorrectly_Implemented_Types
+{
+	[TestClass]
+	public class DefaultGetHashCode
+	{
+		[TestMethod]
+		public void EqualValues()
+		{
+			((Action) (() => ValueTypeAssertions.HasValueEquality(new C(1), new C(1))))
+				.ShouldThrow<AssertFailedException>()
+				.And.Message.Should().Contain("GetHashCode()");
+		}
+
+		private class C
+		{
+			public C(int x)
+			{
+				X = x;
+			}
+
+			public readonly int X;
+
+			public override bool Equals(object obj)
+			{
+				if (ReferenceEquals(null, obj)) return false;
+				if (ReferenceEquals(this, obj)) return true;
+				if (obj.GetType() != GetType()) return false;
+				return Equals((C) obj);
+			}
+
+			// Oops!
+			//public override int GetHashCode()
+			//{
+			//	return X;
+			//}
+
+			public static bool operator ==(C left, C right)
+			{
+				return Equals(left, right);
+			}
+
+			public static bool operator !=(C left, C right)
+			{
+				return !Equals(left, right);
+			}
+
+			protected bool Equals(C other)
+			{
+				return X == other.X;
+			}
+		}
+	}
+}
