0e47182d603410ce7375776cc14ade9582e085a2
Jay Bazuzi <JAY@BAZUZI.COM>
2016-05-14 18:03:12 -0700

    Null checks are reversed

---
+++ ValueTypeAssertions.Tests/Incorrectly Implemented Types/Object.Equals/NullCheckIsReversed.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (C) 2016 Jay Bazuzi - This software may be modified and distributed under the terms of the MIT license.  See the LICENSE.md file for details.
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ValueTypeAssertions.Tests
+{
+	[TestClass]
+	public class NullCheckIsReversed
+	{
+		[TestMethod]
+		public void EqualValues()
+		{
+			((Action) (() => ValueTypeAssertions.HasValueEquality(new AClass(1), new AClass(1))))
+				.ShouldThrow<AssertFailedException>()
+				.And.Message.Should().Contain("Equals(object null)");
+		}
+
+		private class AClass
+		{
+			public AClass(int x)
+			{
+				X = x;
+			}
+
+			public readonly int X;
+
+			public override bool Equals(object obj)
+			{
+				// Oops!
+				if (ReferenceEquals(null, obj)) return true;
+				if (ReferenceEquals(this, obj)) return true;
+				if (obj.GetType() != GetType()) return false;
+				return Equals((AClass) obj);
+			}
+
+			public override int GetHashCode()
+			{
+				return X;
+			}
+
+			public static bool operator ==(AClass left, AClass right)
+			{
+				return Equals(left, right);
+			}
+
+			public static bool operator !=(AClass left, AClass right)
+			{
+				return !Equals(left, right);
+			}
+
+			protected bool Equals(AClass other)
+			{
+				return X == other.X;
+			}
+		}
+	}
+}
